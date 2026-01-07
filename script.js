51186dad99553b03e4716687508e5185785a6d13
Fahim Faysal Nirjhar <fahimfaysal1995@gmail.com>
2026-01-07 17:23:52 +0600

    display video section completed

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const nav = document.getElementById("nav");
+const section = document.querySelector("section");
+const btnAll = document.getElementById("btn-all");
 
 function loadButtons() {
   const URL = "https://openapi.programming-hero.com/api/phero-tube/categories";
@@ -12,9 +14,9 @@ function loadButtons() {
     "category": "Music"
 }*/
 const displayCatagory = (data) => {
-  console.log(data);
+  //   console.log(data);
   for (let catagory of data) {
-    console.log(catagory.category);
+    // console.log(catagory.category);
     const menu = catagory.category;
     const div = document.createElement("div");
     div.innerHTML = `
@@ -28,3 +30,70 @@ const displayCatagory = (data) => {
   }
 };
 loadButtons();
+
+function loadVideos() {
+  const URL = "https://openapi.programming-hero.com/api/phero-tube/videos";
+  fetch(URL)
+    .then((response) => response.json())
+    .then((data) => displayVideos(data.videos));
+}
+
+/*{
+    "category_id": "1001",
+    "video_id": "aaaa",
+    "thumbnail": "https://i.ibb.co/L1b6xSq/shape.jpg",
+    "title": "Shape of You",
+    "authors": [
+        {
+            "profile_picture": "https://i.ibb.co/D9wWRM6/olivia.jpg",
+            "profile_name": "Olivia Mitchell",
+            "verified": ""
+        }
+    ],
+    "others": {
+        "views": "100K",
+        "posted_date": "16278"
+    },
+    "description": "Dive into the rhythm of 'Shape of You,' a captivating track that blends pop sensibilities with vibrant beats. Created by Olivia Mitchell, this song has already gained 100K views since its release. With its infectious melody and heartfelt lyrics, 'Shape of You' is perfect for fans looking for an uplifting musical experience. Let the music take over as Olivia's vocal prowess and unique style create a memorable listening journey."
+}*/
+
+btnAll.onclick = loadVideos;
+
+const displayVideos = (videos) => {
+  videos.forEach((video) => {
+    const card = document.createElement("div");
+    card.innerHTML = `
+    <div class="card">
+        <figure>
+          <img
+            class="w-full h-[150px] object-cover rounded"
+            src="${video.thumbnail}"
+            alt="thumbnail"
+          />
+        </figure>
+        <div class="flex gap-3 py-5">
+          <div class="profile">
+            <div class="avatar">
+              <div class="ring-primary ring-offset-base-100 w-8 rounded-full">
+                <img
+                  src="${video.authors[0].profile_picture}"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="intro">
+            <h2 class="font-bold justify-center text-sm">
+             ${video.title}
+            </h2>
+            <p class="flex gap-3 text-[#171717B3] text-sm">
+              ${video.authors[0].profile_name}
+              <img class="w-6" src="icons8-verified-48.png" alt="" />
+            </p>
+            <p class="text-[#171717B3] text-sm">${video.others.views} views</p>
+          </div>
+        </div>
+      </div>
+    `;
+    section.append(card);
+  });
+};
